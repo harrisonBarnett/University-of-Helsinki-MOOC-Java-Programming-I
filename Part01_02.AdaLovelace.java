be0cfdfaca4250d6bdc581fb1bae5398590f5145
hbarnett91 <74162979+hbarnett91@users.noreply.github.com>
2021-02-28 13:18:42 -0500

    Add files via upload

---
+++ Part01_02.AdaLovelace.java
@@ -0,0 +1,13 @@
+
+public class AdaLovelace {
+
+	public static void main(String[] args) {
+		// Write your program here
+		System.out.println("Ada Lovelace");
+	}
+}
+
+// for the time being, take for granted that "public static void main(String[] args) {}"
+// will generally preceed your code in order for it to run. this is your first intro
+// to "boilerplate code" or code that is almost always present in order to make your
+// programs work
