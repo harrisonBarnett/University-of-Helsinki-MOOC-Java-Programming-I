bbcafeabf2f363acdb32f6c9aeefe5d20b5e5332
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-03-03 18:08:26 -0500

    Add files via upload

---
+++ Part01_04.Dinosaur/src/main/java/com/mycompany/part01_04/dinosaur/Dinosaur.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.part01_04.dinosaur;
+
+/**
+ *
+ * @author Harrison
+ */
+public class Dinosaur {
+	public static void main(String[] args) {
+		// Write your program here
+		System.out.println("Once upon a time");
+		System.out.println("there was");
+		System.out.println("a dinosaur");
+	}
+
+}
+
+// use the shortcut "sout" + tab in netbeans to call the .println() method
