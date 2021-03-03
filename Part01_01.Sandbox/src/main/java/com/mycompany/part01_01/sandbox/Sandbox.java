56f478c0e1086eea93207080342966341c2bcfe8
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-03-03 18:07:43 -0500

    Add files via upload

---
+++ Part01_01.Sandbox/src/main/java/com/mycompany/part01_01/sandbox/Sandbox.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.part01_01.sandbox;
+
+/**
+ *
+ * @author Harrison
+ */
+
+public class Sandbox {
+	
+	public static void main(String[] args) {
+		// Write your program here
+		System.out.println("example");
+	}
+}
+
+// the video in the MOOC details how to utilize the .println() function to 
+// send text to the computer screen. use a "sandbox" program to test 
+// concepts from time to time.
