8529b1e5b9001758f64f60f1c4b51dba296dd59d
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-03-03 18:08:16 -0500

    Add files via upload

---
+++ Part01_03.OnceUponATime/src/main/java/com/mycompany/part01_03/onceuponatime/OnceUponATime.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.part01_03.onceuponatime;
+
+/**
+ *
+ * @author Harrison
+ */
+
+public class OnceUponATime {
+	public static void main(String[] args) {
+		// Write your program here
+		
+		System.out.println("Once upon a time");
+		System.out.println("there was");
+		System.out.println("a program");
+		
+	}
+}
