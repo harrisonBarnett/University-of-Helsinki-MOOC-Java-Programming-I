49f89d3ae07fa6e3ba34bce3cdef45f70e7685b7
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-03-03 18:08:08 -0500

    Add files via upload

---
+++ Part01_02.AdaLovelace/src/main/java/com/mycompany/part01_02/adalovelace/AdaLovelace.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.part01_02.adalovelace;
+
+/**
+ *
+ * @author Harrison
+ */
+
+public class AdaLovelace {
+
+	public static void main(String[] args) {
+		// Write your program here
+		System.out.println("Ada Lovelace");
+	}
+}
+
+// for the time being, take for granted that "public static void main(String[] args) {}"
+// will generally preceed your code in order for it to run. this is your first intro
+// to "boilerplate code" or code that is almost always present in order to make your
+// programs work
