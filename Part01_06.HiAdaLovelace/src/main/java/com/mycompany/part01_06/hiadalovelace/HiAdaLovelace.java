1a7cb0d087b2e3bc85f279b02c2a4bb23c304388
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-03-04 22:52:07 -0500

    Add files via upload

---
+++ Part01_06.HiAdaLovelace/src/main/java/com/mycompany/part01_06/hiadalovelace/HiAdaLovelace.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.part01_06.hiadalovelace;
+
+/**
+ *
+ * @author Harrison
+ */
+public class HiAdaLovelace {
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        
+        // Write your code here
+        
+        System.out.println("Hi " + name + "!");
+    }
+}
+
+// insert the variable name into the argument for println()
