60c5a50d1b479fa78e209ebd97b0cc504a4f49a9
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-03-09 11:15:17 -0500

    Add files via upload

---
+++ Part01_10.Story/src/main/java/com/mycompany/part01_10/story/Story.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.part01_10.story;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Harrison
+ */
+public class Story {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        String name = scanner.nextLine();
+        System.out.println("What is their job?");
+        String job = scanner.nextLine();
+        System.out.println("Here is the story: ");
+        System.out.println("Once upon a time there was " + name + ", who was " + job + ".");
+        System.out.println("On the way to work, " + name + " reflected on life.");
+        System.out.println("Perhaps " + name + " will not be a builder forever.");
+    }
+
+}
