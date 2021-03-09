2ca3bce2802061db5aed7ab21d2b985232bce0ad
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-03-09 11:21:55 -0500

    Add files via upload

---
+++ Part01_11.VariousVariables/src/main/java/com/mycompany/part01_11/variousvariables/VariousVariables.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.part01_11.variousvariables;
+
+/**
+ *
+ * @author Harrison
+ */
+public class VariousVariables {
+    
+    public static void main(String[] args) {
+        // MODIFY THESE:
+        
+        int numberOfChicken = 9000; // modify value to the right of the equality sign
+        double baconWeight = 0.1; // "                                              "
+        String tractor = "Zetor"; // "                                              "
+        
+        // DON'T MODIFY THESE:
+        System.out.println("Chicken: ");
+        System.out.println(numberOfChicken);
+        System.out.println("Bacon (kg): ");
+        System.out.println(baconWeight);
+        System.out.println("Tractor: ");
+        System.out.println(tractor);
+        System.out.println("");
+        System.out.println("And finally, a summary: ");
+        System.out.println(numberOfChicken);
+        System.out.println(baconWeight);
+        System.out.println(tractor);
+    }
+}
