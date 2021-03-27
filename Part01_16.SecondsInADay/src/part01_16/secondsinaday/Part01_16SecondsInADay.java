65f3a98435b512293102886f016d646ddab6a42e
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-03-27 13:58:47 -0400

    Add files via upload

---
+++ Part01_16.SecondsInADay/src/part01_16/secondsinaday/Part01_16SecondsInADay.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package part01_16.secondsinaday;
+
+/**
+ *
+ * @author Harrison
+ */
+public class Part01_16SecondsInADay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
