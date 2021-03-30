545033d95e86bdef51808f7f09b8a5160aa6a49f
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-03-30 15:01:38 -0400

    Add files via upload

---
+++ Part01_25.CheckYourIndentation/src/main/java/com/mycompany/part01_25/checkyourindentation/CheckYourIndentation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.part01_25.checkyourindentation;
+
+/**
+ *
+ * @author Harrison
+ */
+public class CheckYourIndentation {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+// be aware of the indentation of your code, especially as it applies to
+// conditional statements. while the code will still compile and run, it is
+// better to organize codeblocks with proper indentation
+// in Netbeans, the command to automatically format your code is:
+// "alt + shift + f"
